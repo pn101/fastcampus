a5cd30f04b94ac472d342ee3ca5168bdf96b09ff
Philip Nam <philipnam101@gmail.com>
2016-05-04 23:43:14 +0900

    Indentation applied
    Added 4space indentation according to PEP8.

--- stars.py
+++ stars.py
@@ -1,62 +1,62 @@
 # Code 1.
 def stars(count):
-	for i in range(count):
-		print('*' * (i + 1))
+    for i in range(count):
+        print('*' * (i + 1))
 
 stars(int(input('How many?')))
 
 
 # Code 2a.
 def stars(count):
-	for i in range(count):
-		print((' ' * (i + 1)) + ('*' * (count - i)))
+    for i in range(count):
+        print((' ' * (i + 1)) + ('*' * (count - i)))
 
 stars(int(input('How many?')))
 
 
 # Code 2b.
 def stars(count):
-	for i in range(count):
-		print(('*' * (i + 1)).rjust(10))
+    for i in range(count):
+        print(('*' * (i + 1)).rjust(10))
 
 stars(int(input('How many?')))
 
 
 # Code 3a.
 def stars(count):
-	for i in range(count, 0, -1):
-		print('*' * i)
+    for i in range(count, 0, -1):
+        print('*' * i)
 
 stars(int(input('How many?')))
 
 
 # Code 3b.
 def stars(count):
-	for i in range(count):
-		print('*' * (count - i))
+    for i in range(count):
+        print('*' * (count - i))
 
 stars(int(input('How many?')))
 
 
 # Code 4a.
 def stars(count):
-	for i in range(count, 0, -1):
-		print (('*' * i).rjust(10))
+    for i in range(count, 0, -1):
+        print (('*' * i).rjust(10))
 
 stars(int(input('How many?')))
 
 
 # Code 4b.
 def stars(count):
-	for i in range(count):
-		print((' ' * i) + ('*' * (count - i)))
+    for i in range(count):
+        print((' ' * i) + ('*' * (count - i)))
 
 stars(int(input('How many?')))
 
 
 # Code 4c.
 def stars(count):
-	for i in range(count):
-		print(('*' * (count - i)).rjust(10))
+    for i in range(count):
+        print(('*' * (count - i)).rjust(10))
 
 stars(int(input('How many?')))
