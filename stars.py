163111ba0f71c254d9727a1fa29d217f57af4e80
Philip Nam <philipnam101@gmail.com>
2016-05-04 20:16:47 +0900

    Stars Edit
    3a. and 4a. have been added.
    
    Discovered the range(x,y,z) function!

--- stars.py
+++ stars.py
@@ -22,7 +22,15 @@ def stars(count):
 stars(int(input('How many?')))
 
 
-# Code 3.
+# Code 3a.
+def stars(count):
+	for i in range(count, 0, -1):
+		print('*' * i)
+
+stars(int(input('How many?')))
+
+
+# Code 3b.
 def stars(count):
 	for i in range(count):
 		print('*' * (count - i))
@@ -32,13 +40,21 @@ stars(int(input('How many?')))
 
 # Code 4a.
 def stars(count):
+	for i in range(count, 0, -1):
+		print (('*' * i).rjust(10))
+
+stars(int(input('How many?')))
+
+
+# Code 4b.
+def stars(count):
 	for i in range(count):
 		print((' ' * i) + ('*' * (count - 1)))
 
 stars(int(input('How many?')))
 
 
-# Code 4b.
+# Code 4c.
 def stars(count):
 	for i in range(count):
 		print(('*' * (count - i)).rjust(10))
